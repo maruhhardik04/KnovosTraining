f4577262d49578b6b3d30e37bd5d45892db7814a
Hardik <maruhardik04@gmail.com>
2023-01-18 16:36:43 +0530

    Work done on 18/1/23

--- src/main/java/Logger/Main.java
+++ src/main/java/Logger/Main.java
@@ -1,23 +1,39 @@
 package Logger;
 
-import java.util.logging.Level;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Main {
 
 
 
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+    private static FileHandler fileHandler;
 
+    static {
+        try {
+            fileHandler = new FileHandler("Main.log");
+            LOGGER.addHandler(fileHandler);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+        } catch (IOException e) {
+            LOGGER.warning("Failed to create log file.");
+        }
+    }
 
     public static void main(String[] args) {
 
-        Logger logger=Logger.getLogger(Main.class.getName());
 
-        logger.info("This is level  info logging");
 
-        logger.warning("This is level warning logging");
+        LOGGER.info("This is level  info logging");
 
-        logger.severe("This level server logging");
+        LOGGER.warning("This is level warning logging");
+
+        LOGGER.severe("This level server logging");
 
 
 
